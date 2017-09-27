415a36c5876b8acd10ee9ac8f5cb21cad7769e18
Flo <floo2105@gmail.com>
2017-09-27 06:11:53 +0200

    new

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 content = navigator.platform;
 console.log(content);
 if (content.match('Mac')) {
-    document.getElementById("download").innerHTML = '<p> <a href="installers/Swiffle_latest.zip" id="osx" download="swiffle.dmg.zip">Download for OSX</a></p>';
+    document.getElementById("download").innerHTML = '<p> <a href="installers/Swiffle_latest.zip" id="osx" download="Swiffle_latest.zip">Download for OSX</a></p>';
 } else if (content.match('Win')) {
-    document.getElementById("download").innerHTML = '<p> <a href="installers/Swiffle_latest.zip" id="osx" download="swiffle.exe.zip">Download for Windows</a></p>';
+    document.getElementById("download").innerHTML = '<p> <a href="installers/Swiffle_latest.zip" id="osx" download="Swiffle_latest.zip">Download for Windows</a></p>';
 } else if (content.match('Linux')) {
-    document.getElementById("download").innerHTML = '<p> <a href="installers/Swiffle_latest.zip" id="osx" download="swiffle.sh">Download for Linux</a></p>';
+    document.getElementById("download").innerHTML = '<p> <a href="installers/Swiffle_latest.zip" id="osx" download="Swiffle_latest.zip">Download for Linux</a></p>';
 }
